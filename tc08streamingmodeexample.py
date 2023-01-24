9aee02231cfde5fb875191b7830237105c59bbde
huuanito <jonnykent@gmail.com>
2023-01-23 22:00:31 -0800

    Fix seg fault when 1 reading entered. Move prerequisites to another file

--- tc08streamingmodeexample.py
+++ tc08streamingmodeexample.py
@@ -9,6 +9,7 @@
 # PicoTech TC-08 Python STREAMING MODE EXAMPLE
 # original at https://github.com/picotech/picosdk-python-wrappers/blob/master/usbtc08Examples/tc08StreamingModeExample.py
 # modified by waanito to run on macos 10.15 (probably will run on 10.16 but not tested)
+# also modified to work more like the C/C++ version https://github.com/picotech/picosdk-c-examples/blob/master/usbtc08/usbtc08Con/usbtc08Con.c which shows continuously but w/o the single reading which will do in a separate file and also without keyboard interrupt
 
 # SDK reference: https://www.picotech.com/download/manuals/usb-tc08-thermocouple-data-logger-programmers-guide.pdf
 
@@ -21,7 +22,6 @@ from picosdk.usbtc08 import usbtc08 as tc08
 from picosdk.functions import assert_pico2000_ok
 
 USBTC08_MAX_CHANNELS = 8
-channelsInUse = 2
 
 # numberOfReadings = 0 # Number of readings to collect in streaming mode # not used
 readingsCollected = 0 # Number of readings collected at a time in streaming mode
@@ -49,19 +49,16 @@ assert_pico2000_ok(status["set_mains"])
 typeK = ctypes.c_int8(75)
 typeT = ctypes.c_int8(84)
 
-# alias channel numbers and buffers for readability
-# if u don't care about memory use, when using only a couple chennels, could just use a buffer with all 9 channels to avoid buffer indices being different from channel indices
-coldJn = 0
+# alias channel numbers for readability
+# it's memory wasteful, when using only a couple chennels, but using a buffer with all 9 channels avoids buffer indices being different from channel indices
+cjn = 0
 ch1 = 1
 ch7 = 7
-# temp buffer indexes
-coldBufX = 0
-ch1BufX = 1
-ch7BufX = 2
 
 
-#usb_tc08_set_channel(tc08handle, ch, type)
-status["set_channel"] = tc08.usb_tc08_set_channel(chandle, coldJn, typeT); #setting to anything other than ' ' enables cold junction
+# usb_tc08_set_channel(tc08handle, ch, type)
+# enable cold junction
+status["set_channel"] = tc08.usb_tc08_set_channel(chandle, cjn, typeT); #setting to anything other than ' ' enables cold junction
 assert_pico2000_ok(status["set_channel"])
 
 # set channels 1 and 7
@@ -92,22 +89,25 @@ assert_pico2000_ok(status["run"])
 
 
 # collect data for 2 thermocouples plus cold junction
-# [rows][columns] N.B. AFAICT one row per measurement, one col per thermocouple
+# [rows][columns] N.B. the guide doesn't say but AFAICT one row per measurement, one col per thermocouple
 # N.B.: rows,cols
-temp_buffer = (ctypes.c_float * readingsToDo * (channelsInUse + 1))() # one extra for cold junction
+temp_buffer = (ctypes.c_float * readingsToDo * (USBTC08_MAX_CHANNELS +1))() # one extra for cold junction
 times_ms_buffer = (ctypes.c_int32 * readingsToDo)()
 overflow = ctypes.c_int16()
+
 # diag
 #print("buffer columns",len(temp_buffer))
-#print("CJN rows",len(temp_buffer[coldBufX]))
-#print("ch1 rows",len(temp_buffer[ch1BufX]))
-#print("ch7 rows",len(temp_buffer[ch7BufX]))
+#print("CJN rows",len(temp_buffer[cjn]))
+#print("ch1 rows",len(temp_buffer[ch1]))
+#print("ch7 rows",len(temp_buffer[ch7]))
 #print("ms len",len(times_ms_buffer))
 
-# output header
+# print output header
 print("                              cycle");
 print("                              read   reading");
 print(" T(ms)  CJN     Ch1     Ch7   index  number");
+
+# collect data
 collectionTime = (status["get_minimum_interval_ms"]/1000 * 3) + 0.1
 collectionTime=1
 for i in range( readingsToDo ):
@@ -129,23 +129,23 @@ for i in range( readingsToDo ):
   cyclesToRead = 0
   while True:
     cyclesToRead += 1
-    readingsCollected = tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[coldBufX]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow), coldJn, 1, 0)
+    readingsCollected = tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[cjn]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow), cjn, 1, 0)
     if readingsCollected != 0:
       break
   if readingsCollected < 0: # < 0 is an error returned
-    print("error reading coldJn")
+    print("error reading cjn")
     #shut down tco8
     tc08.usb_tc08_stop(chandle)
     tc08.usb_tc08_close_unit(chandle)
     quit()
-  totalReadings[coldJn] = totalReadings[coldJn] +  readingsCollected
+  totalReadings[cjn] = totalReadings[cjn] +  readingsCollected
   
   # get channel 1
   readingsCollected = 0
   cyclesToRead = 0
   while True:
     cyclesToRead += 1
-    readingsCollected = tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[ch1BufX]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow), ch1, 1, 0)
+    readingsCollected = tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[ch1]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow), ch1, 1, 0)
     if readingsCollected != 0:
       break
   if readingsCollected < 0: # < 0 is an error returned
@@ -155,13 +155,13 @@ for i in range( readingsToDo ):
     tc08.usb_tc08_close_unit(chandle)
     quit()
   totalReadings[ch1] = totalReadings[ch1] +  readingsCollected
-
+  
   # get channel 7
   readingsCollected = 0
   cyclesToRead = 0
   while True:
     cyclesToRead += 1
-    readingsCollected =  tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[ch7BufX]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow),
+    readingsCollected =  tc08.usb_tc08_get_temp(chandle, ctypes.byref(temp_buffer[ch7]), ctypes.byref(times_ms_buffer), 9, ctypes.byref(overflow),
         ch7, 1, 0)
     if readingsCollected != 0:
       break
@@ -172,26 +172,25 @@ for i in range( readingsToDo ):
     tc08.usb_tc08_close_unit(chandle)
     quit()
   totalReadings[ch7] = totalReadings[ch7] +  readingsCollected
-
+  
   # print this cycle's readings
   readingsToPrint = min( readingsCollected,
                          readingsToDo,
                          (readingsToDo  - readingsDisplayed)
                        )
                        # avoid bad index when readingsCollected > readingsToDo and/or displaying more readings than asked for
+  
   for j in range(readingsToPrint):
     print("%5d" % (times_ms_buffer[j]), end = ' ')
-    print("%5.4f" % (temp_buffer[coldBufX][j]), end = ' ')
-    print("%5.4f" % (temp_buffer[ch1BufX][j]), end = ' ')
-    print("%5.4f" % ( temp_buffer[ch7BufX][j]), end = ' ')
+    print("%5.4f" % (temp_buffer[cjn][j]), end = ' ')
+    print("%5.4f" % (temp_buffer[ch1][j]), end = ' ')
+    print("%5.4f" % ( temp_buffer[ch7][j]), end = ' ')
     print("%3d" % (j), end = ' ')
     readingsDisplayed += 1
     print("%7d" % (readingsDisplayed))
     
-  
-  if totalReadings[coldJn] >= readingsToDo:
+  if totalReadings[cjn] >= readingsToDo:
     break
-
 
 # stop unit
 status["stop"] = tc08.usb_tc08_stop(chandle)
